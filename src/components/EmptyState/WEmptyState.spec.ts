0760c96b3c1881e8f82e425a7b0308b32069ed42
gabaldon <maria@otherplane.com>
2024-08-28 18:26:24 +0200

    feat: implement section layout and empty state

---
+++ src/components/EmptyState/WEmptyState.spec.ts
@@ -0,0 +1,15 @@
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import WEmptyState from './WEmptyState.vue'
+
+describe('WEmptyState', () => {
+  it('renders properly', () => {
+    const wrapper = mount(WEmptyState, {
+      props: {
+        text: 'Empty state',
+      },
+    })
+
+    expect(wrapper.element).toMatchSnapshot()
+  })
+})
---
+++ src/components/EmptyState/WEmptyState.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="grid gap-md justify-center align-middle">
+    <EmptyStateLogo class="w-max" />
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script setup>
+import EmptyStateLogo from '@/assets/svg/empty-state.svg?component'
+defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+})
+</script>
