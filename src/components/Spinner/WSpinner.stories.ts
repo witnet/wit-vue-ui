624fab8706dbb4b71077b798c27df5f72ca6c9cd
gabaldon <maria@otherplane.com>
2024-08-29 16:38:30 +0200

    refactor: move spinner svg to assets

--- src/components/Spinner/WSpinner.stories.ts
+++ src/components/Spinner/WSpinner.stories.ts
@@ -1,11 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/vue3'
 import WSpinner from './WSpinner.vue'
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta: any = {
   title: 'Example/WSpinner',
   component: WSpinner,
-  // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
   argTypes: {},
   args: {}
