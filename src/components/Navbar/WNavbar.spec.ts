d35da71019eef8a8506087057dd28f5f00a8b8cb
gabaldon <maria@otherplane.com>
2024-08-29 09:47:01 +0200

    refactor: update navbar test

--- src/components/Navbar/WNavbar.spec.ts
+++ src/components/Navbar/WNavbar.spec.ts
@@ -32,21 +32,7 @@ describe('WNavbar', () => {
       redirectionActive: false
     }
   })
-  it('renders properly first link', () => {
-    expect(wrapper.html()).toContain('<a href="#stake"')
-  })
-  it('renders properly second link', () => {
-    expect(wrapper.html()).toContain('<a href="#buy"')
-  })
-  it('renders properly third link', () => {
-    expect(wrapper.html()).toContain('<a href="#build"')
-  })
-  it('renders properly pointer icon when active', () => {
-    expect(wrapper.html())
-      .toContain(`<li class="nav-link"><a href="#build" class="text font-[500] flex cursor-pointer sm:text-white-50 relative p-[8px] m-[-8px] pl-[16px]">
-          <transition-stub name="fade" appear="false" persisted="false" css="true"><svg xmlns="http://www.w3.org/2000/svg" width="12" height="13" fill="none" viewBox="0 0 12 13" class="w-sm h-auto absolute top-[16px] left-[4px]">
-              <path fill="#00E2ED" d="M11.17 6.39 5.89 9.44.61 12.48V.29l5.28 3.05z"></path>
-            </svg></transition-stub><span></span><span class="slash text-wit-blue-500">/</span>Build
-        </a></li>`)
+  it('renders properly the component', () => {
+    expect(wrapper.element).toMatchSnapshot()
   })
 })
