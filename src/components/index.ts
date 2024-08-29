bd77df599d38d1f2d46e7345a8279d22ef77b142
gabaldon <maria@otherplane.com>
2024-08-29 10:43:37 +0200

    refactor: order alphabetically

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,11 @@
 import WButton from './Button/WButton.vue'
+import WFooter from './Footer/WFooter.vue'
 import WLink from './Link/WLink.vue'
 import WNavbar from './Navbar/WNavbar.vue'
-import WFooter from './Footer/WFooter.vue'
 
 export default {
   WButton,
+  WFooter,
   WLink,
   WNavbar,
-  WFooter
 }
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
 import './assets/main.css'
 
 import WButton from './components/Button/WButton.vue'
+import WFooter from './components/Footer/WFooter.vue'
 import WLink from './components/Link/WLink.vue'
 import WNavbar from './components/Navbar/WNavbar.vue'
-import WFooter from './components/Footer/WFooter.vue'
 
 export { WLink, WButton, WNavbar, WFooter }
-export type WButtonType = typeof WButton
-export type LinkType = typeof WLink
-export type WNavbar = typeof WNavbar
+export type WButton = typeof WButton
 export type WFooter = typeof WFooter
+export type WLink = typeof WLink
+export type WNavbar = typeof WNavbar
 
 import type { App } from 'vue'
 
 const WComponents = {
   install(app: App) {
     app.component('WButton', WButton)
+    app.component('WFooter', WFooter)
     app.component('WLink', WLink)
     app.component('WNavbar', WNavbar)
-    app.component('WFooter', WFooter)
   }
 }
 
