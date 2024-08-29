941ce557599efec32e347ebccc970f4ec8a87559
gabaldon <maria@otherplane.com>
2024-08-29 12:02:12 +0200

    refactor: delete unnecessary comment

--- src/components/Footer/WFooter.stories.ts
+++ src/components/Footer/WFooter.stories.ts
@@ -10,11 +10,6 @@ const meta: any = {
 
 export default meta
 type Story = StoryObj<typeof meta>
-/*
- *👇 Render functions are a framework specific feature to allow you control on how the component renders.
- * See https://storybook.js.org/docs/api/csf
- * to learn how to use render functions.
- */
 
 export const Footer: Story = {
   render: (args: any) => ({
