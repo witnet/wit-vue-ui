5194c445345ded7db13bde565037c0ee66f6f67c
gabaldon <maria@otherplane.com>
2024-09-03 18:04:34 +0200

    feat: export card and input components

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import './assets/main.css'
 import WButton from './components/Button/WButton.vue'
 export { buttonTypes } from './components/Button/WButton'
 
+import WCard from './components/Card/WCard.vue'
 import WEmptyState from './components/EmptyState/WEmptyState.vue'
 
 import WFooter from './components/Footer/WFooter.vue'
@@ -15,6 +16,7 @@ import WIconRounded from './components/IconRounded/WIconRounded.vue'
 import WIconText from './components/IconText/WIconText.vue'
 export { iconTextPositions } from './components/IconText/WIconText'
 
+import WInput from './components/Input/WInput.vue'
 import WLink from './components/Link/WLink.vue'
 
 import WLoadingPlaceholder from './components/LoadingPlaceholder/WLoadingPlaceholder.vue'
@@ -30,28 +32,16 @@ import WSpinner from './components/Spinner/WSpinner.vue'
 import WSocialsBar from './components/SocialsBar/WSocialsBar.vue'
 export { SOCIAL_URLS } from './components/SocialsBar/WSocialsBar'
 
-export {
-  WButton,
-  WEmptyState,
-  WFooter,
-  WIcon,
-  WIconRounded,
-  WIconText,
-  WLink,
-  WLoadingPlaceholder,
-  WNavbar,
-  WPagination,
-  WSection,
-  WSocialsBar,
-  WSpinner
-}
 
+export { WButton, WCard, WEmptyState, WFooter, WIcon, WIconText, WIconRounded, WInput, WLink, WLoadingPlaceholder, WNavbar, WPagination, WSection, WSocialsBar, WSpinner }
 export type WButton = typeof WButton
+export type WCard = typeof WCard
 export type WEmptyState = typeof WEmptyState
 export type WFooter = typeof WFooter
 export type WIcon = typeof WIcon
 export type WIconRounded = typeof WIconRounded
 export type WIconText = typeof WIconText
+export type WInput = typeof WInput
 export type WLink = typeof WLink
 export type WLoadingPlaceholder = typeof WLoadingPlaceholder
 export type WNavbar = typeof WNavbar
@@ -65,11 +55,13 @@ import type { App } from 'vue'
 const WComponents = {
   install(app: App) {
     app.component('WButton', WButton)
+    app.component('WCard', WCard)
     app.component('WEmptyState', WEmptyState)
     app.component('WFooter', WFooter)
     app.component('WIcon', WIcon)
     app.component('WIconRounded', WIconRounded)
     app.component('WIconText', WIconText)
+    app.component('WInput', WInput)
     app.component('WLink', WLink)
     app.component('WLoadingPlaceholder', WLoadingPlaceholder)
     app.component('WNavbar', WNavbar)
